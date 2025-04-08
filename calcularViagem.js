cc45c102938ce8a88782f21694a4cccc856ec2af
FernandaCidade <email@email.com>
2025-04-08 15:26:03 -0300

    :construction: Inserido arquivo CSS
    Finalizado a programação do JavaScript. Iniciado a estilização inserindo um arquivo .css

--- calcularViagem.js
+++ calcularViagem.js
@@ -13,4 +13,22 @@ function calcularViagem() {
 
     // Exibir o resultado
     // Aqui alocaremos o resultado dentro da tabela
+
+    var tabela = document.getElementById("resultado").getElementsByTagName("tbody")[0];
+
+    // Aqui será criado uma nova linha dentro da tabela.
+
+    var novaLinha = tabela.insertRow();
+    
+    // Criar as células e adicionar os valores
+    var celulaNome = novaLinha.insertCell(0);
+    var celulaPassageiros = novaLinha.insertCell(1);
+    var celulaValorViagem = novaLinha.insertCell(2);
+    var celulaTotal = novaLinha.insertCell(3);
+
+    // Preencher as células com as informações
+    celulaNome.textContent = nome;
+    celulaPassageiros.textContent = quantidadePassageiros;
+    celulaValorViagem.textContent = valorViagem.toFixed(2);
+    celulaTotal.textContent = resultado.toFixed(2);
 }
